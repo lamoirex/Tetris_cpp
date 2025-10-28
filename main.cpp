71f48c75b6b86924767c17679b873f04dcc3d5d6
lamoirex <ilya-burmistrov-2007@mail.ru>
2025-10-29 00:11:51 +0300

    combined everything in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,115 @@
-#include <iostream>
-#include "pieces.h"
-#include "board.h"
+#include "game.h"
+#ifndef LINUX
+#include <windows.h>
+#endif
 
-int main(void) {
+//int main(void) 
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    // ------ Vars ------
+    IO mIO;
+    int mScreenHeight = mIO.getScreenHeight();
+
+    // Pieces
+    Pieces tetrisBlocks;
+
+    // Board
+    Board mBoard(&tetrisBlocks, mScreenHeight);
+
+    // Game
+    Game mGame(&mBoard, &tetrisBlocks, &mIO, mScreenHeight);
+
+    // Get the actual clock milliseconds (SDL)
+    unsigned long mTime1 = SDL_GetTicks();
     
+    // ------ Main Loop ------
+    while (!mIO.isKeyDown(SDLK_ESCAPE))
+    {
+        // ------ Draw ------
+        mIO.clearScreen(); // Clear screen
+        mGame.drawScene(); // Draw staff
+        mIO.updateScreen(); // Put the graphic context in the screen
+        
+        // ------ Input ------
+        int mKey = mIO.pollKey();
+
+        switch(mKey)
+        {
+            case(SDLK_RIGHT):
+            {
+                if(mBoard.isPossibleMovement(mGame.mPosX + 1, mGame.mPosY, mGame.mPiece, mGame.mRotation))
+                    mGame.mPosX++;
+                break;
+            }
+            case(SDLK_LEFT):
+            {
+                if(mBoard.isPossibleMovement(mGame.mPosX - 1, mGame.mPosY, mGame.mPiece, mGame.mRotation))
+                    mGame.mPosX--;
+                break;
+            }
+            case(SDLK_DOWN):
+            {
+                if(mBoard.isPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation))
+                    mGame.mPosY++;
+                break;
+            }
+            case(SDLK_x):
+            {
+                // Check the collision from up to down
+                while(mBoard.isPossibleMovement(mGame.mPosX, mGame.mPosY, mGame.mPiece, mGame.mRotation)) {
+                    mGame.mPosY++;
+                }
+
+                mBoard.storePiece(mGame.mPosX, mGame.mPosY - 1, mGame.mPiece, mGame.mRotation);
+
+                mBoard.deletePossibleLines();
+
+                if(mBoard.isGameOver())
+                {
+                    mIO.getKey();
+                    exit(0);
+                }
+
+                mGame.createNewPiece();
+
+                break;
+            }
+            case(SDLK_z):
+            {
+                if(mBoard.isPossibleMovement(mGame.mPosX, mGame.mPosY, mGame.mPiece, (mGame.mRotation + 1) % 4))
+                    mGame.mRotation = (mGame.mRotation + 1) % 4; 
+                break;                
+            }
+        }
+        // ------ Vertical movement ------
+
+        unsigned long mTime2 = SDL_GetTicks();
+
+        if((mTime2 - mTime1) > WAIT_TIME)
+        {
+            if(mBoard.isPossibleMovement(mGame.mPosX, mGame.mPosY + 1, mGame.mPiece, mGame.mRotation))
+            {
+                mGame.mPosY++;
+            }
+            else
+            {
+                mBoard.storePiece(mGame.mPosX, mGame.mPosY, mGame.mPiece, mGame.mRotation);
+                
+                mBoard.deletePossibleLines();
+
+                if(mBoard.isGameOver())
+                {
+                    mIO.getKey();
+                    exit(0);
+                }
+
+                mGame.createNewPiece();
+            }
+
+            mTime1 = SDL_GetTicks();
+        }
+
+    }
+
     return 0;
 }
